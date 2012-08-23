dc96ec4f144e43ed935bcda64b42865c1ac31939
ccard <ccard@mymail.mines.edu>
2012-08-22 18:22:21 -0600

    First Change to activity without xml

--- HelloAndroid/src/csci498/helloworld/HelloAndroidActivity.java
+++ HelloAndroid/src/csci498/helloworld/HelloAndroidActivity.java
@@ -2,12 +2,21 @@ package csci498.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.content.*;
 
 public class HelloAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("Hello Chris!");
+        setContentView(textView);
     }
+    
 }
