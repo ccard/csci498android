247c54d53aaa1c05b03bf7f5b545dc8579e3c5ca
Chris Card <ccard@mymail.mines.edu>
2012-09-14 12:28:08 -0600

    Added body onCreate per chap 11 step 2

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/RestaurantHelper.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/RestaurantHelper.java
@@ -10,6 +10,7 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
+import android.database.Cursor;
 
 public class RestaurantHelper extends SQLiteOpenHelper
 {
@@ -25,13 +26,14 @@ public class RestaurantHelper extends SQLiteOpenHelper
 	@Override
 	public void onCreate(SQLiteDatabase db)
 	{
-
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT);");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
 	{
-		
+		// no-op, since will not be called until 2nd schema
+		//version exists
 	}
 
 }
