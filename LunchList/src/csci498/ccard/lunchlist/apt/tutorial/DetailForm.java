da456198516d23bd3a61e356043758aff17bf026
Chris Card <ccard@mymail.mines.edu>
2012-09-17 12:58:04 -0600

    added onsave onclikclistener and added initializations for the access fields of the detail_form.xml per chap 12 step 4

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/DetailForm.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/DetailForm.java
@@ -5,17 +5,84 @@
 */
 package csci498.ccard.lunchlist.apt.tutorial;
 
+import csci498.ccard.lunchlist.R;
+import csci498.ccard.lunchlist.Restaurant;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class DetailForm extends Activity 
 {
+	 
+	//these store the access to the fields the user uses to input info
+	private EditText name = null;
+	private EditText address = null;
+	private RadioGroup types = null;
+	private EditText notes = null;
+	private Restaurant current = null;
+	
+	private RestaurantHelper helper;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.activity_main);
-	}
+		setContentView(R.layout.detail_form);
+
+		 helper = new RestaurantHelper(this);
+        
+        //initialize the view items
+        initViewItems();
+        
+        //stores the save button from the main.xml file
+        Button save = (Button)findViewById(R.id.save);
+        
+        //adds and on click listener
+        save.setOnClickListener(onSave);
+        
+    }
+    
+    /**
+     * this initializes the view items edit text, radiobuttons, and autocomplete
+     */
+    private void initViewItems()
+    {
+    	 //initializing the global views to the view from the xml files
+        name = (EditText)findViewById(R.id.name);
+		address = (EditText)findViewById(R.id.addr);
+		types = (RadioGroup)findViewById(R.id.types);
+        notes = (EditText)findViewById(R.id.notes);
+    }
+
+
+	/**
+     * this stores the onclicklistener for the save button
+     */
+    private View.OnClickListener onSave = new View.OnClickListener() {
+		
+		public void onClick(View v) {
+			
+			String type = null;
+			
+			//Determines the type of restaurant and adds the type to r
+			switch(types.getCheckedRadioButtonId())
+			{
+				case R.id.sit_down:
+					type = "sit_down";
+					break;
+				case R.id.take_out:
+					type = "take_out";
+					break;
+				case R.id.delivery:
+					type = "delivery";
+					break;
+			}
+		
+			
+		}
+	};
 
 }
