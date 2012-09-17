a27c691f7ac9b59ca50a06e9624b59b7b3597005
Chris Card <ccard@mymail.mines.edu>
2012-09-17 12:27:46 -0600

    added detailform activity class to folder apt/tutorial per chap 12 step 1

---
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/DetailForm.java
@@ -0,0 +1,21 @@
+/*
+*Chris Card
+*9/17/12
+*
+*/
+package csci498.ccard.lunchlist.apt.tutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity 
+{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.activity_main);
+	}
+
+}
