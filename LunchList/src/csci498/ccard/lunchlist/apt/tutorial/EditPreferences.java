c8c883146baf1e431aa5ad1a780a38a1ef62b282
Chris Card <ccard@mymail.mines.edu>
2012-09-19 11:58:44 -0600

    added EditPreference activity to apt/tutorial per chap 13 step 2

---
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/EditPreferences.java
@@ -0,0 +1,13 @@
+/*
+*Chris Card
+*9/19/12
+*/
+package csci498.ccard.lunchlist.apt.tutorial;
+
+import android.preference.PreferenceActivity;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class EditPreferences extends PreferenceActivity {
+
+}
