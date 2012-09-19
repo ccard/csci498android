8ae82bae2bafe723b6c3e2920e92ff5357ae038a
Chris Card <ccard@mymail.mines.edu>
2012-09-19 12:11:37 -0600

    added preferences.xml and editpreferences activity per chap 13 task 2

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/EditPreferences.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/EditPreferences.java
@@ -4,10 +4,20 @@
 */
 package csci498.ccard.lunchlist.apt.tutorial;
 
+import csci498.ccard.lunchlist.R;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
 import android.app.Activity;
 
+
 public class EditPreferences extends PreferenceActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		addPreferencesFromResource(R.xml.preferences);
+	}
 
 }
