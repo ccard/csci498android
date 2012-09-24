3d288662d0b9dfb9a840e345a98fe90febc694db
Chris Card <ccard@mymail.mines.edu>
2012-09-24 13:10:08 -0600

    added feed activity to apt/tutorial per chap 15 step 5

---
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/FeedActivity.java
@@ -0,0 +1,12 @@
+/*
+ * Chris Card
+ * 9/24/2012
+ * gets the feeds for restaurants
+ */
+package csci498.ccard.lunchlist.apt.tutorial;
+
+import android.app.ListActivity;
+
+public class FeedActivity extends ListActivity {
+
+}
