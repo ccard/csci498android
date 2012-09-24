e2863c6212a5b0b86dac3ce48006893be7cb3af1
Chris Card <ccard@mymail.mines.edu>
2012-09-24 13:28:16 -0600

    -added feedactivity to manifest and activty -added a static feedtask class in feedactivty per chap 15 step 5

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/FeedActivity.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/FeedActivity.java
@@ -5,8 +5,70 @@
  */
 package csci498.ccard.lunchlist.apt.tutorial;
 
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.os.AsyncTask;
+import android.util.Log;
 
 public class FeedActivity extends ListActivity {
+
+	private static class FeedTask extends AsyncTask<String, Void, Void>
+	{
+		private Exception e = null;
+		private FeedActivity activity = null;
+
+		FeedTask(FeedActivity activity)
+		{
+			attach(activity);
+		}
+
+		private void attach(FeedActivity activity)
+		{
+			this.activity = activity;
+		}
+		@Override
+		public Void doInBackground(String... urls)
+		{
+			try{
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpGet getMethod = new HttpGet(urls[0]);
+				ResponseHandler<String> responseHandler = new BasicResponseHandler();
+				String responseBody = client.execute(getMethod, responseHandler);
+
+				Log.d("FeedActivity", responseBody);
+			}catch(Exception e){
+				this.e = e;
+			}
+
+			return (null);
+		}
+
+		@Override
+		public void onPostExecute(Void unused)
+		{
+			if(e == null)
+			{
+				//TODO
+			}
+			else
+			{
+				Log.e("LunchList", "Exception parsing feed", e);
+				activity.goBlooey(e);
+			}
+		}
+
+		
+	}
+	private void goBlooey(Throwable t)
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setTitle("Exception!").setMessage(t.toString()).setPositiveButton("Ok", null).show();
+	}
 
 }
