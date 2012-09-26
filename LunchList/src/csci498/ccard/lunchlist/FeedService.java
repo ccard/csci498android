27838839d429860259c6c6a80d3914ce553b566e
Chris Card <ccard@mymail.mines.edu>
2012-09-26 12:07:04 -0600

    added functionality to feedservice method onhandleintent per chap 16 step 2

--- LunchList/src/csci498/ccard/lunchlist/FeedService.java
+++ LunchList/src/csci498/ccard/lunchlist/FeedService.java
@@ -5,11 +5,16 @@
 */
 package csci498.ccard.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService 
 {
+   public static final String EXTRA_URL = "apt.tutorial.EXTRA_URL";
 
 	public FeedService()
 	{
@@ -20,8 +25,16 @@ public class FeedService extends IntentService
 	@Override
 	protected void onHandleIntent(Intent i) 
 	{
-		// TODO Auto-generated method stub
+		RSSReader reader = new RSSReader();
 
+		try
+		{
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		}
+		catch (Exception e)
+		{
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 
 }
