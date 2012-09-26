21d90339e0fa18e41c743c225afe29fd4eef1a5b
Chris Card <ccard@mymail.mines.edu>
2012-09-26 12:01:47 -0600

    added feedservice class to lunchlist and updated the manifest to with this service per chap 16 step 1

---
+++ LunchList/src/csci498/ccard/lunchlist/FeedService.java
@@ -0,0 +1,27 @@
+/*
+*Chris Card
+*9/26/2012
+*
+*/
+package csci498.ccard.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService 
+{
+
+	public FeedService()
+	{
+		super("FeedService");
+	}
+
+
+	@Override
+	protected void onHandleIntent(Intent i) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
