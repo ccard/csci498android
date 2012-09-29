add137486125662b7bb8fc584aefe8ed93e79ed4
Chris Card <ccard@mymail.mines.edu>
2012-09-29 16:07:54 -0600

    updated detail form to have location manager onlocationchange object to tell when location has changed and update the location on the ui per chap 17 step 5

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/DetailForm.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/DetailForm.java
@@ -10,6 +10,9 @@ import csci498.ccard.lunchlist.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -38,13 +41,16 @@ public class DetailForm extends Activity
 
 	private String restaurantId = null;
 
+    LocationManager locMgr = null;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.detail_form);
+        locMgr = (LocationManager)getSystemService(LOCATION_SERVICE);
 
-		 helper = new RestaurantHelper(this);
+		helper = new RestaurantHelper(this);
         
         //initialize the view items
         initViewItems();
@@ -126,8 +132,43 @@ public class DetailForm extends Activity
             }
             return true;
         }
+        else if (item.getItemId() == R.id.location)
+        {
+            locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, onLocationChange);
+
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
+
+    LocationListener onLocationChange = new LocationListener()
+    {
+        public void onLocationChanged(Location fix)
+        {
+            helper.updateLocation(restaurantId, fix.getLatitude(), fix.getLongitude());
+
+            location.setText(String.valueOf(fix.getLatitude())+", "+String.valueOf(fix.getLongitude()));
+
+            locMgr.removeUpdates(onLocationChange);
+
+            Toast.makeText(DetailForm.this, "Location saved", Toast.LENGTH_LONG).show();
+        }
+
+		public void onProviderDisabled(String arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+    };
 
     private boolean isNetworkAvailable()
     {
@@ -159,6 +200,7 @@ public class DetailForm extends Activity
     public void onPause()
     {
         save();
+        locMgr.removeUpdates(onLocationChange);
         super.onPause();
     }
 
