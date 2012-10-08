14668b871d21205cd3525ccc623892ec551f43f0
Chris Card <ccard@mymail.mines.edu>
2012-10-08 12:11:21 -0600

    added onbootreciever with no functionality, updated the android manifest to give permission to get boot_completed also added onbootreceiver to as a receiver to the manifest per chap 19 step 3

---
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/OnBootReceiver.java
@@ -0,0 +1,19 @@
+/*
+*Chris Card
+*10/8/2012
+*/
+package csci498.ccard.lunchlist.apt.tutorial;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
