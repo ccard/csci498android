9b5a6bcb0ea9be1aaf707fe70d86d608c31ea695
Chris Card <ccard@mymail.mines.edu>
2012-10-17 10:58:15 -0600

    updated onalarmreceiver to use the new preference option and added icon stat_notify_chat per chap 20 step 2

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/OnAlarmReceiver.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/OnAlarmReceiver.java
@@ -4,20 +4,46 @@
 */
 package csci498.ccard.lunchlist.apt.tutorial;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class OnAlarmReceiver extends BroadcastReceiver {
+
+	private static final int NOTIFY_ME_ID = 1337;
 
 	@Override
 	public void onReceive(Context context, Intent intent) 
 	{
-		Intent i = new Intent(context, AlarmActivity.class);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		boolean useNotification = prefs.getBoolean("use_notification", true);
 
-		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		if (useNotification) 
+		{
+			NotificationManager mgr = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-		context.startActivity(i);
+			Notification note = new Notification(R.drawable.stat_notify_chat, "It's time for lunch!", System.currentTimeMillis());
+			PendingIntent i = PendingIntent.getActivity(context,0, new Intent(context, AlarmActivity.class),0);
+
+			note.setLatestEventInfo(context, "LunchList", "It's time for lunch! Aren't you hungry?", i);
+
+			note.flags |= Notification.FLAG_AUTO_CANCEL;
+
+			mgr.notify(NOTIFY_ME_ID, note);	
+		}
+		else
+		{
+			Intent i = new Intent(context, AlarmActivity.class);
+
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+			context.startActivity(i);
+		}
 
 	}
 
