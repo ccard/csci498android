c95ff5d3915503eacad82ce8349b332ee49dab88
Chris Card <ccard@mymail.mines.edu>
2012-10-19 12:36:05 -0600

    updated widgetapp to start a sevice per chap 21 step 6

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/AppWidget.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/AppWidget.java
@@ -1,7 +1,20 @@
+/*
+* Chris Card
+*10/19/12
+*/
 package csci498.ccard.lunchlist.apt.tutorial;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 public class AppWidget extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds)
+	{
+		ctxt.startService(new Intent(ctxt, WidgetService.class));
+	}
 
 }
