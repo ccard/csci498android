c4803447246e7fd2adf49d00366107d7587a441a
Chris Card <ccard@mymail.mines.edu>
2012-10-24 12:40:29 -0600

    added code to appwidget to creat the widget when it is in android 3.0 per chap 22 step 4

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/AppWidget.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/AppWidget.java
@@ -4,10 +4,12 @@
 */
 package csci498.ccard.lunchlist.apt.tutorial;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 
 public class AppWidget extends AppWidgetProvider {
@@ -23,6 +25,23 @@ public class AppWidget extends AppWidgetProvider {
 		{
 			ctxt.startService(new Intent(ctxt, WidgetService.class));
 		}
+	}
+
+	public void onHCUpdate(Context ctxt, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+	{
+		for (int i = 0; i < appWidgetIds.length; i++) 
+		{
+			Intent svcIntent = new Intent(ctxt, ListWidgetService.class);
+
+			svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+			svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+			Intent clickIntent = new Intent(ctxt, DetailForm.class);
+			PendingIntent clickPI = PendingIntent.getActivity(ctxt, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+			widget.setPendingIntentTemplate(R.id.restaurants, clickPI);
+			appWidgetManager.updateAppWidget(appWidgetIds[i], widget);	
+		}
+		super.onUpdate(ctxt, appWidgetManager, appWidgetIds);
 	}
 
 }
