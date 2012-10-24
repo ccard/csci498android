bb6a1dab3715cc4c00e27aaeb74060c553950562
Chris Card <ccard@mymail.mines.edu>
2012-10-24 13:03:16 -0600

    fixed compiler warnings

--- LunchList/src/csci498/ccard/lunchlist/apt/tutorial/ListWidgetService.java
+++ LunchList/src/csci498/ccard/lunchlist/apt/tutorial/ListWidgetService.java
@@ -4,15 +4,17 @@
 */
 package csci498.ccard.lunchlist.apt.tutorial;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.widget.RemoteViewsService;
 
+@TargetApi(11)
 public class ListWidgetService extends RemoteViewsService {
 
 	@Override
 	public RemoteViewsFactory onGetViewFactory(Intent intent) 
 	{
-		return(new ListViewsFactory(this.getApplicationContext, this));
+		return(new ListViewsFactory(this.getApplicationContext(), intent));
 	}
 
 }
